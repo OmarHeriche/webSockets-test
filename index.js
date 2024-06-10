401103820d6f68d108891a63a27a1ceefa914ad1
omar <herricheomar@gmail.com>
2024-06-11 00:55:47 +0100

    33

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ io.on("connection", (socket) => {
         io.emit("chat message", msg);
     });
 });
-
-server.listen(3000, () => {
-    console.log("server running at http://localhost:3000");
+const port = process.env.PORT || 3000;
+server.listen(port, () => {
+    console.log(`server running at http://localhost:${port}`);
 });
